e31ff35a788b9ad117d781fe1d87ad7647332e2d
Michael Faille <michael@faille.pw>
2014-10-26 12:42:56 -0400

    init

---
+++ pointMilieu.go
@@ -0,0 +1,32 @@
+package main
+
+func main() {
+	brenhenham(0,0,10,6)
+
+}
+
+func brenhenham(x1 int, y1 int, x2 int, y2 int) {
+	println("x1 =", x1, ", y1=", y1, ", x2 = ", x2, ", y2 =", y2)
+	dx := x2 - x1
+	dy := y2 - y1
+	incCE := 2 * dy
+	incNE := 2 * dy - 2 * dx
+	d := 2 * dy - dx
+	y := y1
+	println("dx =", dx, ", dy=", dy, ", incCE = ", incCE, ", d =",  d, ", y =", y, "\n" )
+
+
+	for x := x1; x <= x2; x++ {
+		println("x = ", x, ", y = ", y)
+		if d <= 0 {
+			println("if \n d =", d, " , incCE =", incCE, "\n")
+			d += incCE
+		} else {
+			println("else \n d =", d, ",  incCE =", incNE, "\n")
+			d += incNE
+			y++
+		}
+
+	}
+
+}
